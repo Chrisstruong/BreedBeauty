f6026eaef1f71b368147702c8c03a9116c9276c9
chrisstruong <chrisstruong@gmail.com>
2022-12-07 17:42:31 -0500

    Finish create the list and assign 3 pics in each name

--- project-two/src/Components/DogDetails.js
+++ project-two/src/Components/DogDetails.js
@@ -9,8 +9,8 @@ function DogDetails (props) {
     let { nameDog } = useParams ()
 
     const dogArray = ["affenpinscher", "african", "airedale", "akita", "appenzeller", "australian", "basenji", "beagle", "bluetick", "borzoi", "bouvier", "boxer", "brabancon", "briard", "buhund", "bulldog", "bullterrier", "cattledog", "chihuahua", "chow", "clumber", "cockapoo", "collie", "coonhound", "corgi","cotondetulear", "dachshund", "dalmatian", "dane", "deerhound", "dhole", "dingo", "doberman", "elkhound", "entlebucher", "eskimo", "finnish", "frise", "germanshepherd", "golden", "greyhound", "groenendael", "havanese", "hound", "husky", "keeshond", "kelpie", "komondor", "kuvasz", "labradoodle", "labrador", "leonberg", "lhasa", "malamute", "malinois", "maltese", "mastiff", "mexicanhairless", "mix", "mountain", "newfoundland", "otterhound", "ovcharka", "papillon", "pekinese", "pembroke", "pinscher", "pitbull", "pointer", "pomeranian", "poodle", "pug", "puggle", "pyrenees", "redbone", "retriever", "ridgeback", "rottweiler", "saluki", "samoyed", "schipperke", "schnauzer", "segugio", "setter", "sharpei", "sheepdog", "shiba", "shihtzu", "spaniel", "springer", "stbernard", "terrier", "tervuren", "vizsla", "waterdog", "weimaraner","whippet", "wolfhound"]
-    let randomNumInDogArray = Math.floor(Math.random() * dogArray.length +1)
     let randomNumInPic
+
 
 
     async function fetchDogList() {
@@ -18,26 +18,27 @@ function DogDetails (props) {
             const response = await fetch(`https://dog.ceo/api/breed/${nameDog}/images`)
             const dogNewData = await response.json()
             console.log(dogNewData)
-            // randomNumInPic = Math.floor(Math.random() * dogNewData.message.length + 1)
-            // console.log(`Value of randomNuminpic is ${randomNumInPic}`)
+            randomNumInPic = Math.floor(Math.random() * dogNewData.message.length + 1)
+            
             setDogs(dogNewData.message)
         } catch (err) {
             console.log(err)
         }
     }
 
-    
 
     useEffect(() => {
         fetchDogList()
     }, [])
 
 
-
     return (
         <div>
             {dogs ? <>
                 <img className='photo-box' src={dogs[0]} alt=""/>
+                <img className='photo-box' src={dogs[1]} alt=""/>
+                <img className='photo-box' src={dogs[2]} alt=""/>
+                
             </> : <p>Loading dog...</p>  }
         </div>
     )
