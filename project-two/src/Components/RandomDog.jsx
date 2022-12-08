b5059a310ea36edc72bdbca4cc069260efd59ee3
Calathea-Z <zsykes21@gmail.com>
2022-12-08 15:01:40 -0500

    Add

--- project-two/src/Components/RandomDog.jsx
+++ project-two/src/Components/RandomDog.jsx
@@ -34,7 +34,8 @@ function RandomDog (props) {
                 <div className="photo-box">
                     {randomDogPic ? <>
                     <img src={ randomDogPic[randomNumberInRange(0,randomDogPic.length-1)] } alt="" />
-                    <button id="another">Another!</button>
+                    <button onClick={randomDogPic} id="another">Another!</button>
+                    
                     </> : <p>Loading random Pic of Breed....</p>}
                 </div>
             </div>
