dadfd713bd0fec28b25e54e13acac9a1571adcab
Calathea-Z <zsykes21@gmail.com>
2022-12-08 15:54:53 -0500

    Continue style work on BreedPhotos.css and RandomDog.css

--- project-two/src/Components/RandomDog.jsx
+++ project-two/src/Components/RandomDog.jsx
@@ -34,7 +34,7 @@ function RandomDog (props) {
                 <div className="photo-box">
                     {randomDogPic ? <>
                     <img src={ randomDogPic[randomNumberInRange(0,randomDogPic.length-1)] } alt="" />
-                    <button onClick={randomDogPic} id="another">Another!</button>
+                    <button onClick={fetchRandomDogPic} id="another">Another!</button>
                     
                     </> : <p>Loading random Pic of Breed....</p>}
                 </div>
